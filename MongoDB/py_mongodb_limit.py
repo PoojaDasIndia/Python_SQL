53f776aee64e08c1781f3f7e32d71d0dd1bfae90
Pooja <Poojadas93@outlook.com>
2022-07-23 16:05:26 +0530

    change  the format

--- MongoDB/py_mongodb_limit.py
+++ MongoDB/py_mongodb_limit.py
@@ -1,18 +1,17 @@
 import pymongo
 
-#connection with mongoclient
-myclient=pymongo.MongoClient("localhost",27017)
+# connection with mongoclient
+myclient = pymongo.MongoClient("localhost", 27017)
 
-#database created
-mydb=myclient["EmployeeDetail"]
+# database created
+mydb = myclient["EmployeeDetail"]
 
-#create Collection or Table
+# create Collection or Table
 
-mycoll=mydb['EmployeeDemo']
+mycoll = mydb['EmployeeDemo']
 
 for doc in mycoll.find().limit(5):
     print(doc)
 
-
 for doc in mycoll.find().limit(-3):
-    print(doc)    
+    print(doc)
